876b8e17f8b1a3e82d08b95d158b99274008b548
Choi Sung Sik <css9596@naver.com>
2020-02-09 21:45:36 +0900

    JPA Auditing으로 생성시간/수정시간 자동화

--- src/main/java/com/sik/project/domain/BaseTimeEntity.java
+++ src/main/java/com/sik/project/domain/BaseTimeEntity.java
@@ -23,8 +23,8 @@ import java.time.LocalDateTime;
 public class BaseTimeEntity {
 
     @CreatedDate    //Entity가 생성되어 저장될 때 시간이 자동 저장
-    private LocalDateTime createLocalDate;
+    private LocalDateTime createdDate;
 
     @LastModifiedDate   //조회한 Entity의 값을 변경할 때 시간이 자동 저장
-    private LocalDateTime modifiedDateTime;
+    private LocalDateTime modifiedDate;
 }
