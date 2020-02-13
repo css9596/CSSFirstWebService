53b26ec88af116228cdbe5ea93d407f918deb659
Choi Sung Sik <css9596@naver.com>
2020-02-14 00:32:13 +0900

    Mustache로 수정 기능 추가.

--- src/main/java/com/sik/project/web/IndexController.java
+++ src/main/java/com/sik/project/web/IndexController.java
@@ -9,10 +9,12 @@
 package com.sik.project.web;
 
 import com.sik.project.service.posts.PostsService;
+import com.sik.project.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -29,5 +31,13 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
